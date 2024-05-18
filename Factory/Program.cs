4a931941e5c73a8c8a074c3ab6f739a96c2694f1
Fornalmeister <64432550+Fornalmeister@users.noreply.github.com>
2024-05-18 14:03:12 +0200

    Add project files.

---
+++ Factory/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Factory;
+
+var shapeFactory = new ShapeFactory();
+
+var circle = shapeFactory.CreateShape(ShapeType.Circle);
+circle.Render();
+
+var triangle = shapeFactory.CreateShape(ShapeType.Triangle);
+triangle.Render();
+
+var rectangle = shapeFactory.CreateShape(ShapeType.Rectangle);
+rectangle.Render();
